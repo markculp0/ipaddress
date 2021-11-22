25902c892b4323fddb20e0c8efe466b1f0a34ff5
Mark Culp <markculp0@gmail.com>
2021-11-21 21:43:21 -0600

    roxygen

--- R/functions.R
+++ R/functions.R
@@ -1,26 +1,35 @@
 
 
 
-# convert decimal ip to dot decimal string
+#' Convert decimal ip to dot decimal string
+#'
+#' @param ip_dec_vect Decimal IP to convert
+#' @return ip_dot_vect Dot decimal string
+#' @export
 dec2dot <- function(ip_dec_vect) {
   ip_dot_vect <- character(length = length(ip_dec_vect))
-  
+
   for(i in 3:0) {
     ip_dot_vect <- paste(ip_dot_vect,
                          as.character(ip_dec_vect %/% 256^i),
                          sep = "")
     ip_dec_vect <- ip_dec_vect %% 256^i
-    if (i > 0) ip_dot_vect <- paste(ip_dot_vect, 
+    if (i > 0) ip_dot_vect <- paste(ip_dot_vect,
                                     ".", sep = "")
   }
   ip_dot_vect
 }
 
-# calculate hosts and last ip from cidr
+#' Calculate hosts and last ip from cidr
+#'
+#' @param ip_dec_vect Vector of decimal IP addresses
+#' @param prefix_vect Vector of cidr network prefixes
+#' @return last_ip_dec_vector Vector of decimal last ips
+#' @export
 calc_last_ip_dec <- function(ip_dec_vect, prefix_vect){
   n <- 32 -prefix_vect
   hosts <- 2^n
   last_ip_dec_vector <- ip_dec_vect + hosts - 1
-  
+
   last_ip_dec_vector
 }
